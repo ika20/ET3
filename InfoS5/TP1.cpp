f195f5c46d9f676308c607440dd8eb8665e8bb20
ika20 <91940193+ika20@users.noreply.github.com>
2025-09-05 09:22:12 +0200

    Create TP1.cpp

---
+++ InfoS5/TP1.cpp
@@ -0,0 +1,23 @@
+// Exercice 1
+int main() {
+    int a=12;
+    cout << "Variable = " << a << endl;
+
+    return 0;
+}
+
+// Exercice 2
+int main() {
+    char nom;
+    char prenom;
+    int age;
+    cout << "Saisissez la premiere lettre de votre nom : ";
+    cin >> nom;
+    cout << "Saisissez la premiere lettre de votre prenom : ";
+    cin >> prenom;
+    cout << "Saisissez votre age : ";
+    cin >> age;
+    cout << "Bonjour " << prenom << "." << nom << "., vous avez " << age << " ans.";
+    return 0;
+}
+
