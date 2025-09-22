69ce04fad81c32c37e5a0c46a57fc10f0f3b1151
ika20 <91940193+ika20@users.noreply.github.com>
2025-09-22 15:10:36 +0200

    Update TP2_Fourier.cpp

--- InfoS5/TP2_Fourier.cpp
+++ InfoS5/TP2_Fourier.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
-const unsigned int N = 1000;
+const unsigned int N = 4;
+const float PI = 3.14159265359;
 
 
-float a0(float tab[]){
+float a0(double tab[]){
     float somme = 0;
     for(int i=0; i<N; i+=1){
         somme += tab[i];
@@ -15,55 +16,66 @@ float a0(float tab[]){
 }
 
 
-float ap(float tab[], int p){
-    float somme = 0;
+double ap(double tab[], int p){
+    double somme = 0;
     for(int i=0; i<N; i+=1){
-        somme += tab[i]+sin(p*)
+        somme += ((double)tab[i])*cos(((float)p*i*2*PI)/N);
     }
+    return 2*(somme/N);
 }
 
 
 
 
 int main(){
-    float tab1[N];
-    float tab2[N];
-    float tab3[N];
-    float tab4[N];
+    double tab1[N];
+    double tab2[N];
+    double tab3[N];
+    double tab4[N];
 
     // Remplissage des tableaux
     for(int i=0; i<(N/4); i+=1){
-        tab1[i] = sin(i/N);
+        tab1[i] = sin(2*PI*(i/N));
         tab2[i] = 0;
         tab3[i] = 0.2*(i/N);
         tab4[i] = 0;
     }
 
     for(int i=(N/4); i<(N/2); i+=1){
-        tab1[i] = sin(i/N);
+        tab1[i] = sin(2*PI*(i/N));
         tab2[i] = 0;
         tab3[i] = -0.2*(i/N)+100;
         tab4[i] = 50;
     }
 
     for(int i=(N/2); i<((3*N)/4); i+=1){
-        tab1[i] = sin(i/N);
+        tab1[i] = sin(2*PI*(i/N));
         tab2[i] = 50;
         tab3[i] = -0.2*(i/N)+100;
         tab4[i] = 50;
     }
 
     for(int i=((3*N)/4); i<N; i+=1){
-        tab1[i] = sin(i/N);
+        tab1[i] = sin(2*PI*(i/N));
         tab2[i] = 50;
         tab3[i] = 0.2*(i/N)-200;
         tab4[i] = 50;
     }
 
+    // Calcul de a0 pour chaque fonction
     cout << "a01 : " << a0(tab1) << endl;
     cout << "a02 : " << a0(tab2) << endl;
     cout << "a03 : " << a0(tab3) << endl;
     cout << "a04 : " << a0(tab4) << endl;
+
+    // Calcul de ap pour chaque fonction
+    int p = 1;
+    cout << "ap1 : " << ap(tab1, p) << endl;
+    cout << "ap2 : " << ap(tab2, p) << endl;
+    cout << "ap3 : " << ap(tab3, p) << endl;
+    cout << "ap4 : " << ap(tab4, p) << endl;
+
+
 
 
 
